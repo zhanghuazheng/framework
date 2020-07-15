4af2406ab3fec74401d8b64c64ec9f2ae0b41386
zhanghuazheng <zhanghzi@dicts.com>
2020-07-15 18:23:18 +0800

    1.AOP练习基于注解

--- src/main/java/com/huazheng/framework/config/AopConfig.java
+++ src/main/java/com/huazheng/framework/config/AopConfig.java
@@ -18,17 +18,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class AopConfig {
 
-    @Before("execution( * com.huazheng.framework.domain.*.*(..))")
+    @Before("execution( * com.huazheng.framework.domain.CaluateImpl.*(..))")
     public void before(){
         System.out.println("方法执行前");
     }
 
-    @After("execution( * com.huazheng.framework.domain.*.*(..))")
+    @After("execution( * com.huazheng.framework.domain.CaluateImpl.*(..))")
     public void after(){
         System.out.println("方法执行后");
     }
 
-    @Around("execution( * com.huazheng.framework.domain.*.*(..))")
+    @Around("execution( * com.huazheng.framework.domain.CaluateImpl.*(..))")
     public void around(ProceedingJoinPoint jp) throws Throwable {
         System.out.println("方法执行后");
         Object proceed = jp.proceed();
